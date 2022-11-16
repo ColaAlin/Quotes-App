6f8a0288c7c18796ff6f9d7e0dbbf6944fe1d4dc
Alin Cola <colaalincristian@gmail.com>
2022-11-16 18:07:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const blockquotes = document.querySelector("#quote");
+const button = document.querySelector("#load-quote");
+const author = document.querySelector("#author");
+
+
+ button.addEventListener('click', loadQuotes);
+
+function loadQuotes() {
+    fetch('https://quote-garden.herokuapp.com/api/v3/quotes/random')
+        .then(response => response.json())
+
+        .then(data => {
+            let quoteText = data.data[0].quoteText;
+            let authorText = data.data[0].quoteAuthor;
+      
+            quote.innerText = quoteText;
+            author.innerText = authorText;
+        });
+ }
